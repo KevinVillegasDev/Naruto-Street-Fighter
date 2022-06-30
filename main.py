b81a8da9877e85ca2682c87091877bb25affa650
Kevin Villegas <kevin.villegas94@gmail.com>
2022-06-30 08:30:10 -0700

    added game loop to keep game open until user exits

--- main.py
+++ main.py
@@ -6,4 +6,13 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Brawler")
+pygame.display.set_caption("Naruto Street Fighter")
+
+# game loop to continuously run game and allow characters to be drawn
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
