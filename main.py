ab4179cc240c6853bd6b6c3dab5a0ae2b47041a9
Kevin Villegas <kevin.villegas94@gmail.com>
2022-06-30 08:37:45 -0700

    added initial background to game and set up display functions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from turtle import back
 import pygame
 
 pygame.init()
@@ -8,11 +9,22 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Naruto Street Fighter")
 
+background_img = pygame.image.load(
+    "assets/images/hiddensandbg.png").convert_alpha()
+
+
+def show_bg():
+    scaled_bg = pygame.transform.scale(
+        background_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0, 0))
+
+
 # game loop to continuously run game and allow characters to be drawn
 run = True
 while run:
+    show_bg()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+    pygame.display.update()
 pygame.quit()
