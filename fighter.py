334f4d48f573175084079d08ae009e1fa8382e23
Kevin Villegas <kevin.villegas94@gmail.com>
2022-07-05 13:40:05 -0700

    added sprites, for loop to render each individual sprite, health bar, and load image function

--- fighter.py
+++ fighter.py
@@ -2,12 +2,34 @@ import pygame
 
 
 class Fighter():
-    def __init__(self, x, y):
+    def __init__(self, x, y, data, sprite_sheet, animation_steps):
+        self.size = data[0]
+        self.image_scale = data[1]
+        self.flip = False
+        self.animation_list = self.load_images(sprite_sheet, animation_steps)
+        self.action = 0
+        self.frame_index = 0
+        self.image = self.animation_list[self.action][self.frame_index]
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
         self.attacking = False
         self.attack_type = 0
+        self.health = 100
+
+    def load_images(self, sprite_sheet, animation_steps):
+        # extract from sprite sheet
+        animation_list = []
+        for y, animation in enumerate(animation_steps):
+            temp_img_list = []
+            for x in range(animation):
+                temp_img = sprite_sheet.subsurface(
+                    x * self.size, y * self.size, self.size, self.size)
+
+                temp_img_list.append(pygame.transform.scale(
+                    temp_img, (self.size * self.image_scale, self.size * self.image_scale)))
+            animation_list.append(temp_img_list)
+        return animation_list
 
     def move(self, screen_width, screen_height, surface, target):
         speed = 10
@@ -56,6 +78,13 @@ class Fighter():
             self.vel_y = 0
             self.jump = False
             dy = screen_height - 59 - self.rect.bottom
+
+        # ensure players face each other
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else:
+            self.flip = True
+
         # update player position
         self.rect.x += dx
         self.rect.y += dy
@@ -63,10 +92,11 @@ class Fighter():
     def attack(self, surface, target):
         self.attacking = True
         attacking_rect = pygame.Rect(
-            self.rect.centerx, self.rect.y, 2 * self.rect.width, self.rect.height)
+            self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
         if attacking_rect.colliderect(target.rect):
-            print("hit")
+            target.health -= 10
         pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
 
     def show(self, surface):
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        surface.blit(self.image, (self.rect.x, self.rect.y))
--- main.py
+++ main.py
@@ -13,8 +13,26 @@ pygame.display.set_caption("Naruto Street Fighter")
 clock = pygame.time.Clock()
 FPS = 60
 
+# define colors
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+
+# define fighter variables
+naruto_size = 52
+naruto_scale = 4
+naruto_data = [naruto_size, naruto_scale]
+
+
 background_img = pygame.image.load(
     "assets/images/finalvalley-1.png").convert_alpha()
+
+naruto_sprites = pygame.image.load(
+    "assets/naruto/sprites/naruto sprites.png").convert_alpha()
+
+# find number of steps in each animation
+naruto_steps = [13, 13, 12, 10, 6, 11, 8, 9, 6]
+naruto2_steps = [13, 13, 12, 10, 6, 11, 8, 9, 6]
 
 
 def show_bg():
@@ -22,10 +40,19 @@ def show_bg():
         background_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.blit(scaled_bg, (0, 0))
 
+# function for health bars
+
+
+def health_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
+
 
 # create fighter instances
-fighter_1 = Fighter(200, 310)
-fighter_2 = Fighter(700, 310)
+fighter_1 = Fighter(200, 310, naruto_data, naruto_sprites, naruto_steps)
+fighter_2 = Fighter(700, 310, naruto_data, naruto_sprites, naruto2_steps)
 
 
 # game loop to continuously run game and allow characters to be drawn
@@ -33,6 +60,8 @@ run = True
 while run:
     clock.tick(FPS)
     show_bg()
+    health_bar(fighter_1.health, 20, 20)
+    health_bar(fighter_2.health, 580, 20)
     fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
     # fighter_2.move()
     fighter_1.show(screen)
